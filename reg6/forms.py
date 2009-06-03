473b1ff581f6b4676609fb95863c8bffb0b4271a
Clint Savage <herlo1@gmail.com>
2009-06-02 22:16:01 -0600

    minor changes

--- reg6/forms.py
+++ reg6/forms.py
@@ -4,7 +4,8 @@ from django.forms import ModelForm
 from django.forms import ValidationError
 from django.contrib.auth.models import User
 from django.contrib.formtools.wizard import FormWizard
-from models import Ticket,Item,Order,Payment
+from models import Ticket,Item,Order
+#,Payment
 
 class TicketForm(forms.ModelForm):
 
@@ -24,8 +25,8 @@ class OrderForm(forms.ModelForm):
         model = Order
         fields = ('first_name', 'last_name', 'email')
 
-class PaymentForm(forms.ModelForm):
-
-    class Meta: 
-        model = Payment
-        fields = ('first_name', 'last_name', 'email')
+#class PaymentForm(forms.ModelForm):
+#
+#    class Meta: 
+#        model = Payment
+#        fields = ('first_name', 'last_name', 'email')
--- reg6/urls.py
+++ reg6/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls.defaults import *
 from scalereg.reg6.views import *
-from scalereg.reg6.forms import TicketForm, ItemForm, OrderForm, PaymentForm
+from scalereg.reg6.forms import TicketForm, ItemForm, OrderForm
 from django.contrib.auth.decorators import user_passes_test
 from django.contrib.formtools.wizard import FormWizard
 
 urlpatterns = patterns('scalereg.reg6.views',
-    (r'^$', RegisterWizard([TicketForm, ItemForm, OrderForm, PaymentForm])),
+    (r'^$', RegisterWizard([TicketForm, ItemForm, OrderForm])),
 )
